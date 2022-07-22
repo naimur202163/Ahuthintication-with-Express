0f52a30a170b509ffe4fe8eb517e2cc01875cdfb
naimur <naimur202163@gmail.com>
2022-07-22 16:51:32 +0600

    all routes are added

--- controller/userController.js
+++ controller/userController.js
@@ -82,3 +82,20 @@ exports.signin = (req, res) => {
     });
   });
 };
+
+exports.getAllUsers = (req, res) => {
+  if (!user) {
+    res.status(403).send({
+      message: "Invalid JWT token",
+    });
+  }
+  if (req.user == "admin") {
+    res.status(200).send({
+      message: "Congratulations! but there is no hidden content",
+    });
+  } else {
+    res.status(403).send({
+      message: "Unauthorised access",
+    });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,10 @@
-const { Router } = require("express");
 const express = require("express");
-const { signin, signup } = require("../controller/userController");
-
+const { signin, signup, getAllUsers } = require("../controller/userController");
+const verifyToken = require("../middleware/authJWT");
 const router = express.Router();
 
-router.route('/register').post(signup)
-router.route('/login').post(signin);
+router.route("/register").post(signup);
+router.route("/login").post(signin);
+router.route("/hiddencontent").get(getAllUsers);
 
 module.exports = router;
