e7d3eed40924dff2c98fb92f2c446d13f6817ecf
naimur <naimur202163@gmail.com>
2022-07-22 16:20:03 +0600

    router added

--- controller/userController.js
+++ controller/userController.js
@@ -11,8 +11,7 @@ exports.signup = (req, res, next) => {
     password: bcrypt.hashSync(req.body.password, 8),
   });
 
-  // Saving data on mongoose
-
+  // Saving data on mongoose\
   user.save((err, user) => {
     if (err) {
       res.status(500).send({
@@ -24,5 +23,62 @@ exports.signup = (req, res, next) => {
         message: "User Registerd Successfully",
       });
     }
+  });
+};
+
+exports.signin = (req, res) => {
+  User.findOne({
+    email: req.body.email,
+  }).exec((err, user) => {
+    // When We got errors
+    if (err) {
+      res.status(500).send({
+        message: err,
+      });
+      return;
+    }
+
+    // If user not find
+    if (!user) {
+      return res.status(404).send({
+        message: "user not found",
+      });
+    }
+
+    // Comapring Password
+    let passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+
+    // checking if password was valid and send response accordingly
+
+    if (!passwordIsValid) {
+      return res.status(404).send({
+        accessToken: null,
+        message: "Invalid Password",
+      });
+    }
+
+    //signing token with user id
+
+    let token = jwt.sign(
+      {
+        id: user.id,
+      },
+
+      process.env.API_SECRET,
+      {
+        expiresIn: 86400,
+      }
+    );
+
+    res.status(200).send({
+      user: {
+        id: user._id,
+        email: user.email,
+        fullName: user.fullName,
+      },
+
+      message:"Login successfully",
+      accessToken:token
+    });
   });
 };
---
+++ routes/userRoutes.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+const express = require("express");
+
+const router = express.Router();
+
+
+
+
+module.exports=router
